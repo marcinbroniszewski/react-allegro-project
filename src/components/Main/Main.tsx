a9b6b5d2af454a2f1e993b5a9a9f845b4a4b6d8b
Marcin Broniszewski <mk.broniszewski@gmail.com>
2023-03-24 09:07:42 +0100

    dummy offers section on homepage added

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Carousel from './Carousel/Carousel'
 import Trending from './ProductSections/Trending/Trending'
+import Offers from './ProductSections/Offers/Offers'
 
 export default function Main() {
 	return (
 		<main>
 			<Carousel />
 			<Trending />
+			<Offers />
 		</main>
 	)
 }
